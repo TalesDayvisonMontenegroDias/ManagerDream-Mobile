100988118afca3ebb1cfefced469c5a336d99f06
Tales Dayvison Montenegro Dias <tales.dayvison@gmail.com>
2016-10-25 22:00:00 -0200

    Data inserido ou nao

--- app/src/main/java/com/managerdream/managerdream_mobile/database/DatabaseHelper.java
+++ app/src/main/java/com/managerdream/managerdream_mobile/database/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.managerdream.managerdream_mobile.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -10,6 +11,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper{
     public static final String DATABASE_NAME = "managerDream.db";
+    public static final int DATABASE_VERSION = 1;
     public static final String USER_TABLE_NAME = "user_managerDream_table";
     public static final String USER_ID_TABLE = "user_id_table";
     public static final String USER_CREDIT_TABLE = "user_credit_table";
@@ -21,7 +23,7 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public static final String COST_PAYMENTDAY_TABLE = "cost_paymentDay_table";
 
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 1);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
         SQLiteDatabase db = this.getWritableDatabase();
     }
 
@@ -35,5 +37,17 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + USER_CREDIT_TABLE);
         onCreate(db);
+    }
+
+    public boolean insertData(String credit){
+       SQLiteDatabase db = this.getWritableDatabase();
+       ContentValues contentValues = new ContentValues();
+       contentValues.put(USER_CREDIT_TABLE, credit);
+
+       long result = db.insert(USER_TABLE_NAME,null,contentValues);
+       if(result == -1)
+          return false;
+       else
+          return true;
     }
 }
--- app/src/main/java/com/managerdream/managerdream_mobile/views/Initial_Screen.java
+++ app/src/main/java/com/managerdream/managerdream_mobile/views/Initial_Screen.java
@@ -8,20 +8,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.managerdream.managerdream_mobile.R;
 import com.managerdream.managerdream_mobile.database.DatabaseHelper;
 
 public class Initial_Screen extends AppCompatActivity {
     DatabaseHelper dbHelper;
+    EditText userCredit;
+    Button btnAddData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial__screen);
         dbHelper = new DatabaseHelper(this);
+
+        //userCredit = (EditText)findViewById(R.id.edituser_Credit);
+        //btnAddData = (Button) findViewById(R.id.button_add);
+        AddData();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +40,20 @@ public class Initial_Screen extends AppCompatActivity {
         });
     }
 
+    public void AddData(){
+        btnAddData.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = dbHelper.insertData("userCredit.getText(),toString()");
+                        if(isInserted = true)
+                            Toast.makeText(Initial_Screen.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(Initial_Screen.this, "Data Not Inserted", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
